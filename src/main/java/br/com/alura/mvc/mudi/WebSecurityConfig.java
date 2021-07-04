c94b6f30bbf1cf1769e135e89c0536b63a1c678a
jean Pierre <jean.pierre988@hotmail.com>
2021-07-04 13:05:34 -0300

    Login form

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -20,7 +20,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .httpBasic();
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll());
     }
 
     @Bean

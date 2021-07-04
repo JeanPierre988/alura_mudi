bf9c9c0b74448bb19c6e9cb5ea2fc99c3856cda0
jean Pierre <jean.pierre988@hotmail.com>
2021-07-04 12:06:46 -0300

    First commit

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,84 @@
+package br.com.alura.mvc.mudi.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nomeProduto;
+    private BigDecimal precoProduto;
+    private LocalDate dataEntregaProduto;
+    private String urlProduto;
+    private String urlImagemProduto;
+    private String descricaoProduto;
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(BigDecimal precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public LocalDate getDataEntregaProduto() {
+        return dataEntregaProduto;
+    }
+
+    public void setDataEntregaProduto(LocalDate dataEntregaProduto) {
+        this.dataEntregaProduto = dataEntregaProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagemProduto() {
+        return urlImagemProduto;
+    }
+
+    public void setUrlImagemProduto(String urlImagemProduto) {
+        this.urlImagemProduto = urlImagemProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+}

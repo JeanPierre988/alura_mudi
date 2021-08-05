d81f070e6d1d7070f3df756f49687a7b39789ac5
jean Pierre <jean.pierre988@hotmail.com>
2021-08-04 22:23:20 -0300

    Added Login Page

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/LoginController.java
@@ -0,0 +1,15 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "login";
+    }
+
+}
